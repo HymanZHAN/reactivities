29c48af623495ef3627db59da1f03b622a292d98
Xucong Zhan <zhanxucong@gmail.com>
2022-01-03 19:29:43 -0500

    FB Login completed

--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
-import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { Button, Container, Divider, Header, Image, Segment } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import LoginForm from "../users/LoginForm";
 import RegisterForm from "../users/RegisterForm";
@@ -12,12 +12,7 @@ export default observer(function HomePage() {
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
         <Header as="h1" inverted>
-          <Image
-            size="massive"
-            src="/assets/logo.png"
-            alt="logo"
-            style={{ marginBottom: 12 }}
-          />
+          <Image size="massive" src="/assets/logo.png" alt="logo" style={{ marginBottom: 12 }} />
           Reactivities
         </Header>
         {userStore.isLoggedIn ? (
@@ -29,20 +24,23 @@ export default observer(function HomePage() {
           </>
         ) : (
           <>
-            <Button
-              onClick={() => modalStore.openModal(<LoginForm />)}
-              size="huge"
-              inverted
-            >
+            <Button onClick={() => modalStore.openModal(<LoginForm />)} size="huge" inverted>
               Login
             </Button>
-            <Button
-              onClick={() => modalStore.openModal(<RegisterForm />)}
-              size="huge"
-              inverted
-            >
+            <Button onClick={() => modalStore.openModal(<RegisterForm />)} size="huge" inverted>
               Register
             </Button>
+            <Divider horizontal inverted>
+              OR
+            </Divider>
+            <Button
+              loading={userStore.fbLoading}
+              size="huge"
+              inverted
+              color="facebook"
+              content="Login with Facebook"
+              onClick={userStore.facebookLogin}
+            />
           </>
         )}
       </Container>
